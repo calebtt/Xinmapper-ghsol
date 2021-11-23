95d93a516498681a97d04d86878f8b3e0a25adf8
calebtt <mounteriemachine@hotmail.com>
2021-11-23 05:48:33 -0600

    marked XinSettings members constexpr where applicable.

--- XinSettings.h
+++ XinSettings.h
@@ -11,36 +11,36 @@ namespace sds
 	{
 		//Pixels Magnitude is the number of pixels each iteration of the loop
 		//sends in ThumbstickAxisThread
-		static const int PIXELS_MAGNITUDE = 1;
+		constexpr static const int PIXELS_MAGNITUDE = 1;
 		//Pixels Nomove is the number of pixels each iteration of the loop
 		//sends in ThumbstickAxisThread for the opposite axis the thread is concerned with.
-		static const int PIXELS_NOMOVE = 0;
+		constexpr static const int PIXELS_NOMOVE = 0;
 		//Thread Delay Micro is the value in microseconds the worker thread in
 		//ThumbstickAxisThread sleeps when the previous iteration performed no action.
-		static const int THREAD_DELAY_MICRO = 1500;
+		constexpr static const int THREAD_DELAY_MICRO = 1500;
 		//SMax is the value of the Microsoft type "SHORT"'s maximum possible value.
-		static const short SMax = std::numeric_limits<SHORT>::max();
+		constexpr static const short SMax = std::numeric_limits<SHORT>::max();
 		//SMin is the value of the Microsoft type "SHORT"'s minimum possible value.
-		static const short SMin = std::numeric_limits<SHORT>::min();
+		constexpr static const short SMin = std::numeric_limits<SHORT>::min();
 		//Sensitivity Min is the minimum mouse sensitivity value allowed, used in several places.
-		static const int SENSITIVITY_MIN = 1;
+		constexpr static const int SENSITIVITY_MIN = 1;
 		//Sensitivity Max is the maximum mouse sensitivity value allowed, used in several places.
-		static const int SENSITIVITY_MAX = 100;
+		constexpr static const int SENSITIVITY_MAX = 100;
 		//Sensitivity Default is the default sensitivity value if none is given.
-		static const int SENSITIVITY_DEFAULT = 35;
+		constexpr static const int SENSITIVITY_DEFAULT = 35;
 		//Microseconds Min is the minimum delay for the thumbstick axis thread loop at the highest thumbstick value.
-		static const int MICROSECONDS_MIN = 500;
+		constexpr static const int MICROSECONDS_MIN = 500;
 		//Microseconds Max is the maximum delay for the thumbstick axis thread loop at the lowest thumbstick value.
-		static const int MICROSECONDS_MAX = 10000;
+		constexpr static const int MICROSECONDS_MAX = 10000;
 		//Deadzone Min is the minimum allowable value for a thumbstick deadzone.
-		static const int DEADZONE_MIN = 1;
+		constexpr static const int DEADZONE_MIN = 1;
 		//Deadzone Max is the maximum allowable value for a thumbstick deadzone.
 		constexpr static const int DEADZONE_MAX = std::numeric_limits<SHORT>::max() - 1;
 		//Deadzone Default is the default deadzone value for a thumbstick.
-		static const int DEADZONE_DEFAULT = XINPUT_GAMEPAD_RIGHT_THUMB_DEADZONE;
+		constexpr static const int DEADZONE_DEFAULT = XINPUT_GAMEPAD_RIGHT_THUMB_DEADZONE;
 		//Move Thread Sleep Micro is the delay in microseconds for the XInputBoostMouse work thread loop
 		//the value determines how often the axis threads are told to process a new state (pair of x,y values).
-		static const int MOVE_THREAD_SLEEP_MICRO = 4000;
+		constexpr static const int MOVE_THREAD_SLEEP_MICRO = 4000;
 		//Multiplier Min is the minimum alt deadzone multiplier value allowed.
 		constexpr static const float MULTIPLIER_MIN = 0.01f;
 		//Multiplier Max is the maximum alt deadzone multiplier value allowed.
