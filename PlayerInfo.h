ce4cc53c3328a1e1d065f38d49b574871c737c80
calebtt <mounteriemachine@hotmail.com>
2021-11-23 06:13:24 -0600

    updated playerinfo to incorporate move constructor, and move-assignment operator

--- PlayerInfo.h
+++ PlayerInfo.h
@@ -19,15 +19,17 @@ namespace sds
 		std::atomic<int> left_trigger_dz;
 		std::atomic<int> right_trigger_dz;
 		std::atomic<int> player_id;
+		//default ctor
 		PlayerInfo() 
 			: left_x_dz(XINPUT_GAMEPAD_LEFT_THUMB_DEADZONE),
 			left_y_dz(XINPUT_GAMEPAD_LEFT_THUMB_DEADZONE),
 			right_x_dz(XINPUT_GAMEPAD_RIGHT_THUMB_DEADZONE),
 			right_y_dz(XINPUT_GAMEPAD_RIGHT_THUMB_DEADZONE),
-			player_id(0),
 			left_trigger_dz(XINPUT_GAMEPAD_TRIGGER_THRESHOLD),
-			right_trigger_dz(XINPUT_GAMEPAD_TRIGGER_THRESHOLD)
+			right_trigger_dz(XINPUT_GAMEPAD_TRIGGER_THRESHOLD),
+			player_id(0)
 		{}
+		//copy constructor
 		PlayerInfo(const PlayerInfo &sp)
 		{
 			//temporary copies must be made to copy atomic to atomic
@@ -48,7 +50,28 @@ namespace sds
 			right_trigger_dz = rtdz;
 			player_id = pid;
 		}
+		//move constructor
+		PlayerInfo(PlayerInfo &&sp) noexcept
+		{
+			//temporary copies must be made to copy atomic to atomic
+			int ldzx, ldzy, rdzx, rdzy, ltdz, rtdz, pid;
+			ldzx = sp.left_x_dz;
+			ldzy = sp.left_y_dz;
+			rdzx = sp.right_x_dz;
+			rdzy = sp.right_y_dz;
+			ltdz = sp.left_trigger_dz;
+			rtdz = sp.right_trigger_dz;
+			pid = sp.player_id;
 
+			left_x_dz = ldzx;
+			left_y_dz = ldzy;
+			right_x_dz = rdzx;
+			right_y_dz = rdzy;
+			left_trigger_dz = ltdz;
+			right_trigger_dz = rtdz;
+			player_id = pid;
+		}
+		//assignment
 		PlayerInfo &operator=(const PlayerInfo &sp)
 		{
 			//temporary copies must be made to copy atomic to atomic
@@ -61,6 +84,29 @@ namespace sds
 			rtdz = sp.right_trigger_dz;
 			pid = sp.player_id;
 			
+			left_x_dz = ldzx;
+			left_y_dz = ldzy;
+			right_x_dz = rdzx;
+			right_y_dz = rdzy;
+			left_trigger_dz = ltdz;
+			right_trigger_dz = rtdz;
+			player_id = pid;
+
+			return *this;
+		}
+		//Move assignment operator
+		PlayerInfo &operator=(PlayerInfo &&sp) noexcept
+		{
+			//temporary copies must be made to copy atomic to atomic
+			int ldzx, ldzy, rdzx, rdzy, ltdz, rtdz, pid;
+			ldzx = sp.left_x_dz;
+			ldzy = sp.left_y_dz;
+			rdzx = sp.right_x_dz;
+			rdzy = sp.right_y_dz;
+			ltdz = sp.left_trigger_dz;
+			rtdz = sp.right_trigger_dz;
+			pid = sp.player_id;
+
 			left_x_dz = ldzx;
 			left_y_dz = ldzy;
 			right_x_dz = rdzx;
