4805ebb34a1fe96897884db27b69524f12e681fe
calebtt <mounteriemachine@hotmail.com>
2021-11-28 10:14:08 -0600

    Added xml comment.

--- MouseMap.h
+++ MouseMap.h
@@ -2,6 +2,9 @@
 #include "stdafx.h"
 namespace sds
 {
+	/// <summary>
+	/// Used to denote which thumbstick is to be used for mouse movement.
+	/// </summary>
 	enum class MouseMap : int
 	{
 		NEITHER_STICK,
