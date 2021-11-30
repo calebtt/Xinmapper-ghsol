d0a8f7ab34f5a1c7587a877c75c65ff14b01a44e
calebtt <mounteriemachine@hotmail.com>
2021-11-30 10:07:30 -0600

    Improved TestMouse, added resource file with "description" properties.

--- XNMTest/TestMouse.h
+++ XNMTest/TestMouse.h
@@ -19,18 +19,18 @@ namespace XNMTest
 		TEST_METHOD(TestSetSensitivity)
 		{
 			//test some good cases
-			Assert::IsTrue(mouse.SetSensitivity(1).size() == 0);
-			Assert::IsTrue(mouse.SetSensitivity(100).size() == 0);
-			Assert::IsTrue(mouse.SetSensitivity(50).size() == 0);
+			Assert::IsTrue(mouse.SetSensitivity(1).empty());
+			Assert::IsTrue(mouse.SetSensitivity(100).empty());
+			Assert::IsTrue(mouse.SetSensitivity(50).empty());
 			//test some edge cases
-			Assert::IsTrue(mouse.SetSensitivity(-1).size());
-			Assert::IsTrue(mouse.SetSensitivity(0).size());
-			Assert::IsTrue(mouse.SetSensitivity(101).size());
+			Assert::IsFalse(mouse.SetSensitivity(-1).empty());
+			Assert::IsFalse(mouse.SetSensitivity(0).empty());
+			Assert::IsFalse(mouse.SetSensitivity(101).empty());
 			//test some edge cases for integer value extremes
 			constexpr const long long IntMax = std::numeric_limits<int>::max();
 			constexpr const long long IntMin = std::numeric_limits<int>::min();
-			Assert::IsTrue(mouse.SetSensitivity(IntMax).size());
-			Assert::IsTrue(mouse.SetSensitivity(IntMin).size());
+			Assert::IsFalse(mouse.SetSensitivity(IntMax).empty());
+			Assert::IsFalse(mouse.SetSensitivity(IntMin).empty());
 
 		}
 	};
